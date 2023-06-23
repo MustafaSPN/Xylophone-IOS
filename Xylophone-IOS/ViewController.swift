83c0f600e2ea96abcf6060aa18c9ca7e45ab3e90
MustafaSPN <mustafaspn@hotmail.com>
2023-06-24 00:25:05 +0300

    fix

--- Xylophone-IOS/ViewController.swift
+++ Xylophone-IOS/ViewController.swift
@@ -16,53 +16,15 @@ class ViewController: UIViewController {
           
     }
 
-
-    @IBAction func CPressed(_ sender: UIButton) {
-      loadSound(ResourceName: "C")
-        playSound()
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(ResourceName: sender.currentTitle!)
     }
     
-    @IBAction func DPressed(_ sender: UIButton) {
-        loadSound(ResourceName: "D")
-          playSound()
+    func playSound(ResourceName :String) {
+         let soundURL = Bundle.main.url(forResource: ResourceName, withExtension: "wav",subdirectory: "Sounds")
+             audioPlayer = try! AVAudioPlayer(contentsOf: soundURL!)
+        audioPlayer?.play()
     }
-    @IBAction func EPressed(_ sender: UIButton) {
-        loadSound(ResourceName: "E")
-          playSound()
-    }
-    @IBAction func FPressed(_ sender: UIButton) {
-        loadSound(ResourceName: "F")
-          playSound()
-    }
-    @IBAction func GPressed(_ sender: UIButton) {
-        loadSound(ResourceName: "G")
-          playSound()
-    }
-    @IBAction func APressed(_ sender: UIButton) {
-        loadSound(ResourceName: "A")
-          playSound()
-    }
-    @IBAction func BPressed(_ sender: UIButton) {
-        loadSound(ResourceName: "B")
-          playSound()
-    }
-    func loadSound(ResourceName :String) {
-        if let soundURL = Bundle.main.url(forResource: ResourceName, withExtension: "wav",subdirectory: "Sounds") {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-            } catch {
-                print("Ses dosyası yüklenemedi.")
-            }
-        } else {
-            print("Ses dosyası bulunamadı.")
-        }
-    }
-    func playSound() {
-        if let player = audioPlayer {
-            player.play()
-        } else {
-            print("Ses dosyası yüklenemedi.")
-        }
-    }
+ 
 }
 
